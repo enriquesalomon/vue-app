0008fc916e8a240abceb3c1422dd850005e7e375
Enrique Salomon <king92salomon@gmail.com>
2022-10-10 16:36:13 +0800

    Adding Form Input Login

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,17 +8,12 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <router-link class="nav-link " aria-current="page" to="/login">Login</router-link>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
+  
       </ul>
     </div>
   </div>
