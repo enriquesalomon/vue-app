5741fcf07a24301b7562266777a9d1bee1da907e
Enrique Salomon <king92salomon@gmail.com>
2022-10-20 17:20:19 +0800

    Re-render componentUpdate list of users after log

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -30,7 +30,9 @@
                         </td>
 
                         <td v-if="u.token.id > 0">
-                            <span class="badge bg-success" @click="logUserOut(u.id)">Logged in</span>
+                            <a href="javascript:void(0);">
+                                <span class="badge bg-success" @click="logUserOut(u.id)">Logged in</span>
+                            </a>
                         </td>
                         <td v-else>
                             <span class="badge bg-danger">Not logged in</span>
@@ -81,24 +83,18 @@ export default {
                 notie.confirm({
                     text: "Are you sure you want to log this user out?",
                     submitText: "Log Out",
-                    submitCallback: function() {
+                    submitCallback: () => {
                         console.log("Would log out user id", id);
                         fetch(process.env.VUE_APP_API_URL + "/admin/log-user-out/" + id, Security.requestOptions(""))
-                        .then((response)=> response.json())
+                        .then((response) => response.json())
                         .then((data) => {
                             if (data.error) {
-                                notie.alert({
-                                    type: 'error',
-                                    text: data.message,
-                                })
+                                this.$emit('error', data.message);
                             } else {
-                                    notie.alert({
-                                        type: 'succes',
-                                        text: data.message,
-                                    })
+                                this.$emit('success', data.message);
+                                this.$emit('forceUpdate');
                             }
                         })
-
                     }
                 })
             } else {
